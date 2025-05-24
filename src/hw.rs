b56217826c4d63fffd1a6b72f539e3094aabd65c
Igor Lesik <curoles@yahoo.com>
2025-05-24 01:20:28 -0700

    experimenting

--- src/hw.rs
+++ src/hw.rs
@@ -1,15 +1,70 @@
+pub trait PortVal: Copy + Default {}
+impl<T: Copy + Default> PortVal for T {}
 
-struct OutputPort<T> {
+#[derive(Default)]
+pub struct OutputPort<T: PortVal> {
     val: T,
 }
 
-struct InputPort<T> {
+pub struct InputPort<T: PortVal> {
     val: T,
     driver: *const OutputPort<T>,
+}
+
+impl<T: PortVal> Default for InputPort<T> {
+    fn default() -> Self {
+        Self {
+            val: Default::default(),
+            driver: std::ptr::null(),
+        }
+    }
 }
 
 #[test]
 fn input_port_construction() {
     let op = OutputPort::<u8>{val: 123};
-    let ip = InputPort::<u8>{val: 7, driver: &op};
+    let _ip = InputPort::<u8>{val: 7, driver: &op};
+}
+
+pub fn connect<T: PortVal>(out: &OutputPort<T>, inp: &mut InputPort<T>) {
+    assert!(inp.driver.is_null());
+    inp.driver = std::ptr::from_ref(out);
+}
+
+#[test]
+fn input_port_connection() {
+    let op = OutputPort::<u8>{val: 123};
+    let mut ip : InputPort::<u8> = Default::default();
+    connect(&op, &mut ip);
+}
+
+pub trait Module {
+    fn run(&mut self);
+}
+
+#[derive(Default)]
+pub struct Dff<T: PortVal> {
+    inp: InputPort<T>,
+    out: OutputPort<T>,
+}
+
+impl<T: PortVal> Dff<T> {
+    pub fn new() -> Self {
+        Self {..Default::default()}
+    }
+}
+
+impl<T: PortVal> Module for Dff<T> {
+
+    fn run(&mut self) {
+        self.out.val = self.inp.val;
+        //wait(1);
+    }
+}
+
+#[test]
+fn dff_input_port_connection() {
+    let op = OutputPort::<u32>{val: 123};
+    let mut dff : Dff::<u32> = Dff::<u32>::new();
+    connect(&op, &mut dff.inp);//.register(scheduler, dff);
 }
