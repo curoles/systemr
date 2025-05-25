9b2663be2a859ea72647612adf85b4ae6f137f9e
Igor Lesik <curoles@yahoo.com>
2025-05-25 01:34:32 -0700

    experimenting

--- src/hw.rs
+++ src/hw.rs
@@ -11,6 +11,18 @@ pub struct InputPort<T: PortVal> {
     driver: *const OutputPort<T>,
 }
 
+impl<T: PortVal> InputPort<T> {
+    pub fn connect(&mut self, out: &OutputPort<T>) {
+        assert!(self.driver.is_null());
+        self.driver = std::ptr::from_ref(out);
+    }
+
+    pub fn update(&mut self) {
+        assert!(!self.driver.is_null());
+        unsafe { self.val = (*self.driver).val; }
+    }
+}
+
 impl<T: PortVal> Default for InputPort<T> {
     fn default() -> Self {
         Self {
@@ -26,20 +38,30 @@ fn input_port_construction() {
     let _ip = InputPort::<u8>{val: 7, driver: &op};
 }
 
-pub fn connect<T: PortVal>(out: &OutputPort<T>, inp: &mut InputPort<T>) {
-    assert!(inp.driver.is_null());
-    inp.driver = std::ptr::from_ref(out);
-}
-
 #[test]
 fn input_port_connection() {
     let op = OutputPort::<u8>{val: 123};
-    let mut ip : InputPort::<u8> = Default::default();
-    connect(&op, &mut ip);
+    let mut _ip : InputPort::<u8> = Default::default();
+    _ip.connect(&op);
+}
+
+pub struct PortPtrs {
+    driver: *const u8,
+    val: *const u8,
+    size: usize,
 }
 
 pub trait Module {
     fn run(&mut self);
+    fn register_port<T: PortVal>(&mut self, inp: *const InputPort<T>) {
+        //copy_from(self, src: *const T, count: usize), cast_mut
+        let _info = PortPtrs {
+            //driver: std::ptr::addr_of!((*inp).val),
+            driver: unsafe { &raw const (*inp).val as *const u8 },
+            val: unsafe { &raw const (*inp).val as *const u8 },
+            size: size_of::<T>(),
+        };
+    }
 }
 
 #[derive(Default)]
@@ -60,11 +82,13 @@ impl<T: PortVal> Module for Dff<T> {
         self.out.val = self.inp.val;
         //wait(1);
     }
+
 }
 
 #[test]
 fn dff_input_port_connection() {
     let op = OutputPort::<u32>{val: 123};
-    let mut dff : Dff::<u32> = Dff::<u32>::new();
-    connect(&op, &mut dff.inp);//.register(scheduler, dff);
+    let mut dff = Dff::<u32>::new();
+    dff.inp.connect(&op);
+    dff.register_port(&dff.inp);
 }
